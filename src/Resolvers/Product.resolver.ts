3af3ddbff30515aa99334a1cf58986ae2ff9787c
thai-bug <hxt12021999@gmail.com>
2022-03-21 02:09:53 +0700

    update

--- src/Resolvers/Product.resolver.ts
+++ src/Resolvers/Product.resolver.ts
@@ -4,11 +4,13 @@ import { Resolver, Query, Args } from '@nestjs/graphql';
 import { ProductService } from '@Services/Product.service';
 
 @Resolver((of) => Product)
-export class ProductResolver{
+export class ProductResolver {
   constructor(@Inject(ProductService) private productService: ProductService) {}
 
   @Query((returns) => Product)
-  async products(@Args('id') id: number): Promise<Product | null> {
-    return await this.productService.findById(id);
+  async products(@Args('id') id: number): Promise<[Product[], number]> {
+    return await this.productService.getProducts({
+      id: id,
+    });
   }
 }
